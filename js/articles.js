4abaf97908109c3138c0d4bbcacbf5905c5a60cf
alexweb185 <alexnavia.dark@gmail.com>
2022-08-29 01:43:57 -0400

    primer commit

---
+++ js/articles.js
@@ -0,0 +1,35 @@
+[
+    {
+        "id": 1,
+        "title": "base de datos",
+        "parrafo": "This is a wider card with supporting text below as a natural" +
+        "This is a wider card with supporting text below as a natural This is a wider" +
+        "card with supporting text below as a natural" + 
+        "lead-in to additional content. This content is a littl"
+    },
+    {
+        "id": 2,
+        "title": "php",
+        "parrafo": "This is a wider card with supporting text below as a natural" +
+        "This is a wider card with supporting text below as a natural This is a wider" +
+        "card with supporting text below as a natural" + 
+        "lead-in to additional content. This content is a littl"
+    },
+    {
+        "id": 3,
+        "title": "instalar node",
+        "parrafo": "This is a wider card with supporting text below as a natural" +
+        "This is a wider card with supporting text below as a natural This is a wider" +
+        "card with supporting text below as a natural" + 
+        "lead-in to additional content. This content is a littl"
+    },
+
+    {
+        "id": 4,
+        "title": "seo",
+        "parrafo": "This is a wider card with supporting text below as a natural" +
+        "This is a wider card with supporting text below as a natural This is a wider" +
+        "card with supporting text below as a natural" + 
+        "lead-in to additional content. This content is a littl"
+    },
+]
